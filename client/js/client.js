3cf9bc0b6e926f597b02dd39ad408ca390eff1b1
Justin Li <ml.justin.li@gmail.com>
2020-06-03 13:59:50 -0400

    Add stylesheet Add basic setup and draw functions

--- client/js/client.js
+++ client/js/client.js
@@ -3,3 +3,23 @@ var socket = io();
 socket.on("connect", function (data) {
   socket.emit("join");
 });
+
+function setup() {
+  let canvas = createCanvas(480, 640);
+  canvas.parent("game-canvas-wrapper");
+}
+
+function draw() {
+  background(255);
+  fill(0);
+
+  drawBasketballCourt();
+}
+
+function keyPressed() {
+  console.log("Key pressed: " + keyCode);
+}
+
+function keyReleased() {}
+
+function drawBasketballCourt() {}
