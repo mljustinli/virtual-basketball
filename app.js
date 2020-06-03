5225c25d6f0929fdd15f92f45ee3b383e4e2fa83
mljustinli <ml.justin.li@gmail.com>
2020-06-03 10:46:41 -0400

    Fix port error with heroku

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
 app.use(express.static(__dirname + "/client"));
-app.get("/", function (req, res, next) {
-  res.sendFile(__dirname + "/client/index.html");
+app.get("/", function(req, res, next) {
+    res.sendFile(__dirname + "/client/index.html");
 });
 
 let idTracker = 0;
 
-io.on("connection", function (socket) {
-  socket.id = idTracker;
-  idTracker++;
-  console.log("Client connected...");
-  console.log("Client id is: " + socket.id);
-  console.log("IP address is: " + socket.request.connection.remoteAddress);
+io.on("connection", function(socket) {
+    socket.id = idTracker;
+    idTracker++;
+    console.log("Client connected...");
+    console.log("Client id is: " + socket.id);
+    console.log("IP address is: " + socket.request.connection.remoteAddress);
 
-  socket.on("join", function (data) {
-    console.log("Received join message from client");
-  });
+    socket.on("join", function(data) {
+        console.log("Received join message from client");
+    });
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
