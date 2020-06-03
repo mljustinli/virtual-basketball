29d775fdc93be6508b4d7386caa51fa255367fc2
Justin Li <ml.justin.li@gmail.com>
2020-06-03 15:27:20 -0400

    Add way to input initials. It's not pretty, but it gets the job done

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
 app.use(express.static(__dirname + "/client"));
-app.get("/", function(req, res, next) {
-    res.sendFile(__dirname + "/client/index.html");
+app.get("/", function (req, res, next) {
+  res.sendFile(__dirname + "/client/index.html");
 });
 
 let idTracker = 0;
 
-io.on("connection", function(socket) {
-    socket.id = idTracker;
-    idTracker++;
-    console.log("Client connected...");
-    console.log("Client id is: " + socket.id);
-    console.log("IP address is: " + socket.request.connection.remoteAddress);
+io.on("connection", function (socket) {
+  socket.id = idTracker;
+  idTracker++;
+  console.log("Client connected...");
+  console.log("Client id is: " + socket.id);
+  console.log("IP address is: " + socket.request.connection.remoteAddress);
 
-    socket.on("join", function(data) {
-        console.log("Received join message from client");
-    });
+  socket.on("join", function (data) {
+    console.log("Received join message from client");
+  });
+
+  socket.on("initials", function (data) {
+    // TODO associate initials with player object
+    socket.initials = data;
+    console.log("Received player initials: " + socket.initials);
+  });
 });
 
 server.listen(process.env.PORT || 3000);
---
+++ client/js/inputInitials.js
@@ -0,0 +1,27 @@
+var input = $("#initials-input");
+
+$(document).ready(function () {
+  input.keypress(function (event) {
+    if (event.keyCode == 13) {
+      let initials = input.val();
+      console.log("Initials are: " + initials);
+      event.preventDefault();
+
+      // send server the initials
+      socket.emit("initials", initials);
+
+      // hide the input and show the list of games/game
+      $("#initials-input-wrapper").addClass("hidden");
+      $("#game-canvas-wrapper").removeClass("hidden");
+    }
+  });
+
+  if (!promptForInitials) {
+    // send server the initials
+    socket.emit("initials", "AA");
+
+    // hide the input and show the list of games/game
+    $("#initials-input-wrapper").addClass("hidden");
+    $("#game-canvas-wrapper").removeClass("hidden");
+  }
+});
