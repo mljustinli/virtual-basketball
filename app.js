5b44009e584b9db4b2954bbcf3e54704eb56e684
Justin Li <ml.justin.li@gmail.com>
2020-06-03 12:06:33 -0400

    Add files for janky movement tutorial

--- app.js
+++ app.js
@@ -5,22 +5,51 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
 app.use(express.static(__dirname + "/client"));
-app.get("/", function(req, res, next) {
-    res.sendFile(__dirname + "/client/index.html");
+app.get("/", function (req, res, next) {
+  res.sendFile(__dirname + "/client/index.html");
 });
 
 let idTracker = 0;
 
-io.on("connection", function(socket) {
-    socket.id = idTracker;
-    idTracker++;
-    console.log("Client connected...");
-    console.log("Client id is: " + socket.id);
-    console.log("IP address is: " + socket.request.connection.remoteAddress);
+let socketList = {};
+let currSocket;
 
-    socket.on("join", function(data) {
-        console.log("Received join message from client");
-    });
+io.on("connection", function (socket) {
+  socket.id = idTracker;
+  socketList[socket.id] = socket;
+  currSocket = socket;
+  socket.emit("giveID", socket.id);
+  socket.pos = { x: 0, y: 240 };
+  idTracker++;
+  console.log("Client connected...");
+  console.log("Client id is: " + socket.id);
+  console.log("IP address is: " + socket.request.connection.remoteAddress);
+
+  socket.on("join", function (data) {
+    console.log("Received join message from client");
+  });
+
+  socket.on("updatePos", function (data) {
+    // console.log("receiving updatepos message");
+    socketList[data.id].pos.y -= data.delta;
+    console.log(socketList[data.id].pos.y);
+  });
 });
+
+setInterval(function () {
+  let playerPositions = {};
+  for (let key in socketList) {
+    playerPositions[key] = socketList[key].pos;
+  }
+
+  for (let key in socketList) {
+    socketList[key].emit("update", playerPositions);
+  }
+
+  // if (currSocket) {
+  //   console.log("curr socket exists");
+  //   currSocket.emit("update", playerPositions);
+  // }
+}, 1000 / 30);
 
 server.listen(process.env.PORT || 3000);
--- client/js/client.js
+++ client/js/client.js
@@ -1,5 +1,68 @@
 var socket = io();
 
+let id;
+let playerPositions;
+let movingUp = false;
+let movingDown = false;
+
 socket.on("connect", function (data) {
   socket.emit("join");
+
+  console.log("Whoooo we connected");
 });
+
+socket.on("giveID", function (data) {
+  id = data;
+  console.log("Given id is: " + id);
+});
+
+socket.on("update", function (data) {
+  console.log("receive update");
+  playerPositions = data;
+});
+
+/**
+ * Game Stuff
+ */
+
+function setup() {
+  createCanvas(480, 480);
+  ellipseMode(CENTER);
+}
+
+function draw() {
+  background(255);
+  fill(0);
+  if (playerPositions) {
+    for (let key in playerPositions) {
+      rect(playerPositions[key].x, playerPositions[key].y, 20, 20);
+    }
+  }
+
+  if (movingUp) {
+    socket.emit("updatePos", { id: id, delta: 4 });
+  }
+  if (movingDown) {
+    socket.emit("updatePos", { id: id, delta: -4 });
+  }
+}
+
+function keyPressed() {
+  console.log(keyCode);
+
+  if (keyCode == 87) {
+    movingUp = true;
+  }
+  if (keyCode == 83) {
+    movingDown = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode == 87) {
+    movingUp = false;
+  }
+  if (keyCode == 83) {
+    movingDown = false;
+  }
+}
