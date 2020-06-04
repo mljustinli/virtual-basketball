5ce74be660e151227f8bd23c1670013efae94a82
skolczynski28 <seank1@rochester.rr.com>
2020-06-04 10:28:39 -0400

    team differences

--- client/js/chooseTeam.js
+++ client/js/chooseTeam.js
@@ -1,6 +1,12 @@
 var button = $("#team-choice-red");
 
-function clickage() {
+function clickage(color) {
+  if (color == 'red') {
+    console.log("RED TEAM");
+  }
+  if (color == 'blue') {
+    console.log("BLUE TEAM");
+  }
   console.log("Button pushed!");
   $("#team-choice-wrapper").addClass("hidden");
   $("#game-canvas-wrapper").removeClass("hidden");
