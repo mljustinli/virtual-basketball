1347e49aa8e9d260aa0192756f4abb4c888501f4
skolczynski28 <seank1@rochester.rr.com>
2020-06-04 09:47:54 -0400

    cleaning up

--- client/js/chooseTeam.js
+++ client/js/chooseTeam.js
@@ -1,19 +1,20 @@
-var input = $("#team-choice");
+var input = $("#team-choice-red");
 
-function clickage() {
-  background(255, 204, 0);
-  fill(0);
-  console.log("Button pushed!");
-  $("#team-choice-wrapper").addClass("hidden");
-  $("#game-canvas-wrapper").removeClass("hidden");
-};
+// function clickage() {
+//   background(255, 204, 0);
+//   fill(0);
+//   console.log("Button pushed!");
+//   $("#team-choice-wrapper").addClass("hidden");
+//   $("#game-canvas-wrapper").removeClass("hidden");
+// };
 
 $(document).ready(function() {
-  input.keypress(console.log"pushed!");
+
+  // input.keypress(console.log"pushed!");
 
 
   console.log("game choice");
 
   $("#team-choice-wrapper").addClass("hidden");
-  $("#game-canvas-wrapper").removeClass("hidden");
-};
+  //$("#game-canvas-wrapper").removeClass("hidden");
+});
